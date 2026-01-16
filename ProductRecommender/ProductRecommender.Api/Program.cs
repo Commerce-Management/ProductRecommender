cc04c69fd7509eb4139ad7f3f05ca5656cf94124
Ziya Hacili <magahajili@gmail.com>
2026-01-16 23:13:30 +0400

    add grpc configs to program.cs

--- ProductRecommender/ProductRecommender.Api/Program.cs
+++ ProductRecommender/ProductRecommender.Api/Program.cs
@@ -1,35 +1,100 @@
 using System;
+using System.Net;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProductRecommender.Application;
+using ProductRecommender.Infrastructure;
+using ProductRecommender.Infrastructure.Interfaces;
 using ProductRecommender.Shared.Protos.GrpcOrderService;
 
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddGrpcClient<OrderService.OrderServiceClient>(options =>
-{
-    options.Address = new Uri(builder.Configuration["gRPC:OrderService"]); 
-});
-builder.Services.AddGrpcClient<OrderService.OrderServiceClient>(options =>
-{
-    options.Address = new Uri(builder.Configuration["gRPC:CartService"]); 
-});
-
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// builder.Services.AddGrpcClient<OrderService.OrderServiceClient>(options =>
+// {
+//     options.Address = new Uri(builder.Configuration["gRPC:OrderService"]); 
+// });
+// builder.Services.AddGrpcClient<OrderService.OrderServiceClient>(options =>
+// {
+//     options.Address = new Uri(builder.Configuration["gRPC:CartService"]); 
+// });
+// builder.Services.AddGrpcClient<OrderService.OrderServiceClient>(options =>
+// {
+//     options.Address = new Uri(builder.Configuration["gRPC:ProductService"]); 
+// });
+builder.Services.AddGrpcClient<OrderService.OrderServiceClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["gRPC:RatingService"]); 
+});
+
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Listen(IPAddress.Any, 5268, listenOptions =>
+    {
+        listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http1AndHttp2;
+    });
+    
+
+    options.Listen(IPAddress.Any, 5007, listenOptions =>
+    {
+        listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2; 
+    });
+}); 
+
+
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:3001", "http://localhost:3000", "http://localhost:5040", "http://localhost")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials()
+            .WithExposedHeaders("X-Total-Count", "X-Page-Number", "X-Page-Size");
+    });
+});
+
 builder.Services.AddGrpc();
+builder.Services.AddControllers();
+
+// for AutoMapper
+// builder.Services.AddAutoMapper(cfg => { }, typeof(OrderService.Core.Profiles.OrderProfile));
+
+// for repos and services 
+// builder.Services.AddScoped<IOrderService, OrderService.Application.Services.OrderService>();
+
+// for DB 
+// builder.Services.AddDbContext<OrderDbContext>(options =>
+//     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"),
+//         b => b.MigrationsAssembly("OrderService.Infrastructure"))
+// );
+
+
+builder.Services.AddSingleton<IProductRecommenderModel, ProductRecommenderModel>();
+builder.Services.AddScoped<IRecommendationService, RecommendationServiceApp>();
+
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCookiePolicy();
+app.UseCors();
+
+// app.UseAuthentication();
+// app.UseAuthorization();
+
 
 app.UseHttpsRedirection();
+app.MapControllers();
+
 
 app.Run();
---
+++ ProductRecommender/ProductRecommender.Api/RecommendationsController .cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductRecommender.Application;
+
+namespace ProductRecommender.Api;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class RecommendationsController : ControllerBase
+{
+    private readonly IRecommendationService _recommendationService;
+
+    public RecommendationsController(IRecommendationService recommendationService)
+    {
+        _recommendationService = recommendationService;
+    }
+
+    [HttpGet("{userId:guid}")]
+    public async Task<IActionResult> GetUserRecommendations(Guid userId, [FromQuery] int limit = 10, CancellationToken ct = default)
+    {
+        var items = await _recommendationService.GetRecommendationsForUserAsync(userId, limit, ct);
+        return Ok(items);
+    }
+
+    // endpoint для ручного триггера обучения (для начала)
+    [HttpPost("train")]
+    public async Task<IActionResult> Train(CancellationToken ct = default)
+    {
+        await _recommendationService.TrainModelAsync(ct);
+        return Ok();
+    }
+}
